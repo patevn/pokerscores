06ab9cd35435e99791fc161b3b9abb1938bbd0fb
Peter McNaught <petermmcnaught@gmail.com>
2017-10-24 19:54:21 +1100

    Stringing the bools so they show up in the table

--- src/App.js
+++ src/App.js
@@ -142,10 +142,10 @@ function OutputForm(props) {
         props.week.chunks[props.week.iterator].map((score, index) =>
           <tbody key={index}><tr>
             <td>{score.gameDate}</td>
-            <td><b>AssHole:</b>{score.asshole}</td>
+            <td><b>AssHole:</b>{String(score.asshole)}</td>
             <td><b>Cash Won:</b>{score.cashWon}</td>
             <td><b>Position:</b>{score.position}</td>
-            <td><b>President:</b>{score.president}</td>
+            <td><b>President:</b>{String(score.president)}</td>
             <td><b>Who:</b>{score.who}</td>
           </tr></tbody>
         )
