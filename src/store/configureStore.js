98171d4dd27ee306e857d82522a033b8379cfa6f
Peter McNaught <petermmcnaught@gmail.com>
2018-01-06 13:56:10 +1100

    Created redux store and connected it into the app entry point

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
+//will use configureStore at entry point in app
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
+        applyMiddleware(reduxImmutableStateInvariant())
     );
 }
