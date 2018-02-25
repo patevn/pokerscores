d02fe64e73b51dfea07a2fc90c8f87db3d24f0fc
Peter McNaught <petermmcnaught@gmail.com>
2018-02-25 13:03:05 +1100

    added redux logger so i can see whats going on

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 const initialState = configureStore();
 initialState.dispatch(loadData());
@@ -19,7 +20,7 @@ export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
+        applyMiddleware(thunk, reduxImmutableStateInvariant(), logger)
     );
 }
 
