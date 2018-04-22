22ffa975ccf2e6a5b7490a1902c2a790b23d65c2
Peter McNaught <petermmcnaught@gmail.com>
2018-04-22 11:00:08 +1000

    Cleaned up routes warning

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Route } from 'react-router';
 import App from './containers/App';
 import Menu from './components/Menu';
 
 export default (
-    <Router history={browserHistory}>>
+    <div>
         <Route path='/' component={Menu} />
         <Route path='/app' component={App} />
-    </Router>
+    </div>
 );
