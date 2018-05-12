1b3c666ca38e9e0a7136e2118adfe4a47cb40d95
Peter McNaught <petermmcnaught@gmail.com>
2018-05-12 17:53:13 +1000

    Changed total points name and added current position and send to app

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,12 +3,11 @@ import React from "react";
 export default function Table(props) {
 
     function ListItem(props) {
-
         const tb =
             <tbody>
                 <tr>
                     <th scope="row">{props.value.name}</th>
-                    <td>{props.value.position}</td>
+                    <td>{props.value.totalPoints}</td>
                     <td>{props.value.cashWon}</td>
                     <td>{props.value.president}</td>
                     <td>{props.value.asshole}</td>
@@ -21,8 +20,8 @@ export default function Table(props) {
 
     const listItems = list.map((number, key) =>
         <ListItem key={key} value={number} />
-
     );
+
     return (
         <table className="table table-striped">
             <thead>
@@ -37,5 +36,4 @@ export default function Table(props) {
             {listItems}
         </table>
     );
-
 }
--- src/reducers/totalTemplate.js
+++ src/reducers/totalTemplate.js
@@ -4,35 +4,40 @@ export default class totalTemplate {
             "Matty": {
                 "name": "Matty",
                 "cashWon": 0,
-                "position": 0,
+                "totalPoints": 0,
+                "currentPosition": 0,
                 "asshole": 0,
                 "president": 0
             },
             "Grady": {
                 "name": "Grady",
                 "cashWon": 0,
-                "position": 0,
+                "totalPoints": 0,
+                "currentPosition": 0,
                 "asshole": 0,
                 "president": 0
             },
             "Greg": {
                 "name": "Greg",
                 "cashWon": 0,
-                "position": 0,
+                "totalPoints": 0,
+                "currentPosition": 0,
                 "asshole": 0,
                 "president": 0
             },
             "Brad": {
                 "name": "Brad",
                 "cashWon": 0,
-                "position": 0,
+                "totalPoints": 0,
+                "currentPosition": 0,
                 "asshole": 0,
                 "president": 0
             },
             "Ando": {
                 "name": "Ando",
                 "cashWon": 0,
-                "position": 0,
+                "totalPoints": 0,
+                "currentPosition": 0,
                 "asshole": 0,
                 "president": 0
             }
