e8e9f054c9b9b87fa737f7e5abaeb5945807339e
Peter McNaught <petermmcnaught@gmail.com>
2018-06-19 10:16:25 +1000

    no longer adds your cash if you are pos 6

--- src/reducers/initReducer.js
+++ src/reducers/initReducer.js
@@ -85,35 +85,45 @@ function calculator(fwd, state) {
   fwd.forEach(element => {
     if (element.who === "Matty") {
       totals.Matty.currentPosition = Number(element.position);
-      totals.Matty.cashWon = round(Number(state.totals.Matty.cashWon) + Number(element.cashWon));
+      if(element.position === "6"){
+        totals.Matty.cashWon = round(Number(state.totals.Matty.cashWon) + Number(0));
+      }else{totals.Matty.cashWon = round(Number(state.totals.Matty.cashWon) + Number(element.cashWon));};
       totals.Matty.totalPoints = round(Number(state.totals.Matty.totalPoints) + Number(element.position));
       element.asshole === true ? (totals.Matty.asshole = adder(state.totals.Matty.asshole, element.asshole)) : (totals.Matty.asshole = state.totals.Matty.asshole);
       element.president === true ? (totals.Matty.president = adder(state.totals.Matty.president, element.president)) : (totals.Matty.president = state.totals.Matty.president);
     }
     if (element.who === "Ando") {
       totals.Ando.currentPosition = Number(element.position);
-      totals.Ando.cashWon = round(Number(state.totals.Ando.cashWon) + Number(element.cashWon));
+      if(element.position === "6"){
+        totals.Ando.cashWon = round(Number(state.totals.Ando.cashWon) + Number(0));
+      }else{totals.Ando.cashWon = round(Number(state.totals.Ando.cashWon) + Number(element.cashWon));};
       totals.Ando.totalPoints = round(Number(state.totals.Ando.totalPoints) + Number(element.position));
       element.asshole === true ? (totals.Ando.asshole = adder(state.totals.Ando.asshole, element.asshole)) : (totals.Ando.asshole = state.totals.Ando.asshole);
       element.president === true ? (totals.Ando.president = adder(state.totals.Ando.president, element.president)) : (totals.Ando.president = state.totals.Ando.president);
     }
     if (element.who === "Grady") {
       totals.Grady.currentPosition = Number(element.position);
-      totals.Grady.cashWon = round(Number(state.totals.Grady.cashWon) + Number(element.cashWon));
+      if(element.position === "6"){
+        totals.Grady.cashWon = round(Number(state.totals.Grady.cashWon) + Number(0));
+      }else{totals.Grady.cashWon = round(Number(state.totals.Grady.cashWon) + Number(element.cashWon));};
       totals.Grady.totalPoints = round(Number(state.totals.Grady.totalPoints) + Number(element.position));
       element.asshole === true ? (totals.Grady.asshole = adder(state.totals.Grady.asshole, element.asshole)) : (totals.Grady.asshole = state.totals.Grady.asshole);
       element.president === true ? (totals.Grady.president = adder(state.totals.Grady.president, element.president)) : (totals.Grady.president = state.totals.Grady.president);
     }
     if (element.who === "Greg") {
       totals.Greg.currentPosition = Number(element.position);
-      totals.Greg.cashWon = round(Number(state.totals.Greg.cashWon) + Number(element.cashWon));
+      if(element.position === "6"){
+        totals.Greg.cashWon = round(Number(state.totals.Greg.cashWon) + Number(0));
+      }else{totals.Greg.cashWon = round(Number(state.totals.Greg.cashWon) + Number(element.cashWon));};
       totals.Greg.totalPoints = round(Number(state.totals.Greg.totalPoints) + Number(element.position));
       element.asshole === true ? (totals.Greg.asshole = adder(state.totals.Greg.asshole, element.asshole)) : (totals.Greg.asshole = state.totals.Greg.asshole);
       element.president === true ? (totals.Greg.president = adder(state.totals.Greg.president, element.president)) : (totals.Greg.president = state.totals.Greg.president);
     }
     if (element.who === "Brad") {
       totals.Brad.currentPosition = Number(element.position);
-      totals.Brad.cashWon = round(Number(state.totals.Brad.cashWon) + Number(element.cashWon));
+      if(element.position === "6"){
+        totals.Brad.cashWon = round(Number(state.totals.Brad.cashWon) + Number(0));
+      }else{totals.Brad.cashWon = round(Number(state.totals.Brad.cashWon) + Number(element.cashWon));};
       totals.Brad.totalPoints = round(Number(state.totals.Brad.totalPoints) + Number(element.position));
       element.asshole === true ? (totals.Brad.asshole = adder(state.totals.Brad.asshole, element.asshole)) : (totals.Brad.asshole = state.totals.Brad.asshole);
       element.president === true ? (totals.Brad.president = adder(state.totals.Brad.president, element.president)) : (totals.Brad.president = state.totals.Brad.president);
